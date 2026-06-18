ce1c232aca53e95f21c441896c4298c79d476741
pkli <lpkshuai@gmail.com>
2026-06-18 19:58:13 +0800

    fix: remove basePath for github pages user site

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? "/lpkshuai.github.io" : "",
+  // basePath: process.env.NODE_ENV === "production" ? "/lpkshuai.github.io" : "",
 };
 
 export default nextConfig;
